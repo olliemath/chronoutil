c59f2f9574ef3e94323a576d0a4a4f092dfd63fc
Ingrid <git@ingrids.email>
2024-04-20 22:26:00 +0200

    support nanoseconds in duration parser

--- src/relative_duration/parse.rs
+++ src/relative_duration/parse.rs
@@ -1,8 +1,9 @@
 use super::RelativeDuration;
 use chrono::Duration;
 
-fn dhms_to_duration(days: i32, hours: i32, minutes: i32, seconds: i32) -> Duration {
+fn dhmsn_to_duration(days: i32, hours: i32, minutes: i32, seconds: i32, nanos: i32) -> Duration {
     Duration::seconds((((days * 24 + hours) * 60 + minutes) * 60 + seconds) as i64)
+        + Duration::nanoseconds(nanos.into())
 }
 
 fn get_terminated(input: &str, terminator: char) -> Result<(&str, i32), String> {
@@ -13,6 +14,39 @@ fn get_terminated(input: &str, terminator: char) -> Result<(&str, i32), String> 
         Ok((remainder, int))
     } else {
         Ok((input, 0))
+    }
+}
+
+fn get_terminated_decimal(input: &str, terminator: char) -> Result<(&str, i32, i32), String> {
+    if let Some((decimal_string, remainder)) = input.split_once(terminator) {
+        let (int_string, fraction_string) = decimal_string.split_once('.').unwrap_or_else(|| {
+            decimal_string
+                // if no '.' was found, look for ',', as both are valid decimal separators in iso 8601
+                .split_once(',')
+                // if neither is found take the whole string as the integer part, with no fraction
+                .unwrap_or((decimal_string, ""))
+        });
+
+        let int = int_string
+            .parse::<i32>()
+            .map_err(|_| format!("{} is not a valid i32", int_string))?;
+
+        let fraction = if fraction_string.is_empty() {
+            0
+        } else {
+            fraction_string
+                .chars()
+                // right pad with zeros
+                .chain(std::iter::repeat('0'))
+                // truncate to 9 chars, since we only support nanosecond resolution
+                .take(9)
+                .collect::<String>()
+                .parse::<i32>()
+                .map_err(|_| format!("{} is not a valid i32", fraction_string))?
+        };
+        Ok((remainder, int, fraction))
+    } else {
+        Ok((input, 0, 0))
     }
 }
 
@@ -32,10 +66,10 @@ fn parse_datespec(datespec: &str) -> Result<(i32, i32, i32), String> {
     }
 }
 
-fn parse_timespec(timespec: &str) -> Result<(i32, i32, i32), String> {
+fn parse_timespec(timespec: &str) -> Result<(i32, i32, i32, i32), String> {
     let (remainder, hours) = get_terminated(timespec, 'H')?;
     let (remainder, mins) = get_terminated(remainder, 'M')?;
-    let (remainder, secs) = get_terminated(remainder, 'S')?;
+    let (remainder, secs, nanos) = get_terminated_decimal(remainder, 'S')?;
 
     if !remainder.is_empty() {
         Err(format!(
@@ -43,7 +77,7 @@ fn parse_timespec(timespec: &str) -> Result<(i32, i32, i32), String> {
             remainder, timespec
         ))
     } else {
-        Ok((hours, mins, secs))
+        Ok((hours, mins, secs, nanos))
     }
 }
 
@@ -61,12 +95,14 @@ impl RelativeDuration {
     /// a [`RelativeDuration`] value.
     ///
     /// This supports only duration strings with integer values (i.e `"P1Y"` but not `"P0.5Y"` or
-    /// `"P0,5Y"`), as fractional values cannot be unambiguously represented as a [`RelativeDuration`]
+    /// `"P0,5Y"`), as fractional values cannot be unambiguously represented as a
+    /// [`RelativeDuration`]. The one exception to this is the seconds field, where the fractional
+    /// part is truncated to 9 digits, and parsed as nanoseconds.
     ///
     /// # Errors
     ///
     /// - Invalid duration string input
-    /// - Fractional values in duration string
+    /// - Fractional values (apart from seconds) in duration string
     ///
     /// # Example
     ///
@@ -86,10 +122,10 @@ impl RelativeDuration {
         let (datespec, timespec) = input.split_once('T').unwrap_or((input, ""));
 
         let (years, months, days) = parse_datespec(datespec)?;
-        let (hours, mins, secs) = parse_timespec(timespec)?;
+        let (hours, mins, secs, nanos) = parse_timespec(timespec)?;
 
         Ok(RelativeDuration::months(years * 12 + months)
-            .with_duration(dhms_to_duration(days, hours, mins, secs)))
+            .with_duration(dhmsn_to_duration(days, hours, mins, secs, nanos)))
     }
 
     /// Formats a [`RelativeDuration`] value into an
@@ -151,7 +187,8 @@ mod tests {
             ),
             (
                 "P2Y2M2DT2H2M2S",
-                RelativeDuration::months(2 * 12 + 2).with_duration(dhms_to_duration(2, 2, 2, 2)),
+                RelativeDuration::months(2 * 12 + 2)
+                    .with_duration(dhmsn_to_duration(2, 2, 2, 2, 0)),
             ),
             (
                 "P1M",
@@ -160,6 +197,8 @@ mod tests {
             ("PT10M", RelativeDuration::minutes(10)),
             ("P-1M", RelativeDuration::months(-1)),
             ("P1W1D", RelativeDuration::days(8)),
+            ("PT0.0000000010S", RelativeDuration::nanoseconds(1)),
+            ("PT0.1S", RelativeDuration::nanoseconds(100_000_000)),
         ]
         .iter()
         .for_each(|(input, expected)| {
@@ -175,7 +214,8 @@ mod tests {
                 "P1YT1S",
             ),
             (
-                RelativeDuration::months(2 * 12 + 2).with_duration(dhms_to_duration(2, 2, 2, 2)),
+                RelativeDuration::months(2 * 12 + 2)
+                    .with_duration(dhmsn_to_duration(2, 2, 2, 2, 0)),
                 "P2Y2M2DT2H2M2S",
             ),
             (
